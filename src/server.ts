bf6de89348aa6566938ddb3b841ef97eb0320b96
Fabiano.Vanhois <fabianovanhois@gmail.com>
2021-04-21 12:16:16 -0300

    Config database, criacao migrations, entities, repositorys, controllers

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,24 @@
 import express from "express";
 
+import "./database";
+import { routes } from "./routes";
+
 const app = express ();
 
-app.get("/", (request, response) => {
-    return response.json({
-        message: "Olá NLW 05",
-    });
-});
+// app.get("/", (request, response) => {
+//     return response.json({
+//         message: "Olá NLW 05",
+//     });
+// });
 
-app.post("/", (request, response) => {
-    return response.json({
-        message: "Usuario salvo com sucesso!",
-    });
-});
+// app.post("/", (request, response) => {
+//     return response.json({
+//         message: "Usuario salvo com sucesso!",
+//     });
+// });
+
+app.use(express.json());
+
+app.use(routes);
 
 app.listen(3333, () => console.log('Server is running on port 3333'));
